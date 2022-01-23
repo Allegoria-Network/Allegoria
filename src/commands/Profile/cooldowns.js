36d43297b5514c6181bf9440a78c85f2a7855cbe
Pauldb09 <pauldeboissieu@yahoo.com>
2022-01-23 10:17:12 +0100

    d

--- src/commands/Profile/cooldowns.js
+++ src/commands/Profile/cooldowns.js
@@ -17,27 +17,22 @@ class Help extends Command {
     }
 
     async run(ctx, userDB) {
-        const a = userDB.cooldowns.daily ? userDB.cooldowns.daily : 0
-        const d_1 = moment(a);
-        const d_2 = moment(a + (1000 * 60 * 60 * 12));
+            const a = userDB.cooldowns.daily ? userDB.cooldowns.daily : 0
+            const d_1 = moment(a);
+            const d_2 = moment(a + (1000 * 60 * 60 * 12));
 
-        const b = userDB.cooldowns.miney ? userDB.cooldowns.mine : 0
-        const b_1 = moment(b);
-        const b_2 = moment(b + (1000 * 60 * 30));
+            const b = userDB.cooldowns.mine ? userDB.cooldowns.mine : 0
+            const b_1 = moment(b);
+            const b_2 = moment(b + (1000 * 60 * 30));
 
-        ctx.reply({
-            embeds: [{
+            ctx.reply({
+                        embeds: [{
 
-                fields: category.map(x => {
-                    return {
-                        name: x,
-                        value: ctx.client.commands.commands.filter(cmd => cmd.category === x && !cmd.cmdTest).filter(cmd => cmd.category === x && !cmd.ownerOnly).map(cmd => `\`${cmd.name}\``).join(", ")
-                    };
-                }),
-                color: "#efaa66",
-                author: { name: "Your cooldowns", icon_url: ctx.author.displayAvatarURL({ size: 512, format: "png" }), url: "https://discord.gg/6wa4U5sAMe" },
-                footer: { text: `Tip: Go premium to increase your cooldowns`, icon_url: ctx.client.user.displayAvatarURL({ size: 512, format: "png" }) },
-                description: `__Daily__: \nLast redeem: <t:${d_1.unix()}:R>\nNext redeem: <t:${d_2.unix()}:R>\n__Mine__: \nLast redeem: <t:${b_1.unix()}:R>\nNext redeem: <t:${b_2.unix()}:R>`,
+
+                                    color: "#efaa66",
+                                    author: { name: "Your cooldowns", icon_url: ctx.author.displayAvatarURL({ size: 512, format: "png" }), url: "https://discord.gg/6wa4U5sAMe" },
+                                    footer: { text: `Tip: Go premium to increase your cooldowns`, icon_url: ctx.client.user.displayAvatarURL({ size: 512, format: "png" }) },
+                                    description: `__Daily__: \nLast redeem: ${a==0?"Never claimed":` <t:${d_1.unix()}:R>`}\nNext redeem: ${a==0?"Now!":` <t:${d_2.unix()}:R>`}\n__Mine__: \nLast redeem: ${b==0?"Never claimed":` <t:${b_1.unix()}:R>`}\nNext redeem: ${b==0?"Now!":` <t:${b_2.unix()}:R>`}`,
             }]
         })
     }
--- src/utils/dashboardManager.js
+++ src/utils/dashboardManager.js
@@ -1,22 +1,13 @@
 'use strict';
-const fetch = require('node-fetch');
 class ApiManager {
     constructor(client) {
         this._client = client;
-        this.baseUrl = "https://api.green-bot.app/v2"
-        this.discordApiUrl = "https://discordapp.com/api";
     }
 
     async launch(id, data) {
-        const url = this.baseUrl + "/update?server=" + id;
-        console.log(url);
-        const response = await fetch(url, {
-            method: 'post',
-            body: JSON.stringify(data),
-            headers: { 'Content-Type': 'application/json' }
-        })
-        const newDB = await response.json();
-        return newDB;
+        if (this.client.shard.ids[0] == 0) {
+            cos
+        }
     }
 }
 
