bf25bd60a264efb98e10a24a77971ae59e6138fa
pauldb09 <70904183+pauldb09@users.noreply.github.com>
2022-01-23 15:55:38 +0100

    Delete src directory

--- src/models/errorModel.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const data = new mongoose.Schema({
-    code: { type: String, required: true },
-    error: { type: String, required: true },
-})
-const errorModel = module.exports = mongoose.model('errorModel', data)
--- src/utils/extender.js
+++
@@ -1,9 +0,0 @@
-const { Interaction } = require("discord.js");
-Interaction.prototype.errorMessage = function(msg = {}) {
-    return this.reply({
-        embeds: [{
-            description: "<:reject:929356899336486942> " + msg,
-            color: "#C73829",
-        }]
-    })
-};
