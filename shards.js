16bc81f45b5a6b25000860e2eae220a0de0b6dbc
pauldb09 <70904183+pauldb09@users.noreply.github.com>
2022-03-13 20:03:55 +0100

    Update shards.js

--- shards.js
+++ shards.js
@@ -2,21 +2,16 @@
 
 const { ShardingManager } = require("discord.js");
 const { bot } = require("./config.json");
-const Logger = require('./src/utils/Logger.js');
 const shardManagerLogger = new Logger("ShardingManager");
 shardManagerLogger.info("Sharding manager starting !");
-const process_args = process.argv.slice(2);
 
 new ShardingManager("./main.js", {
     respawn: true,
-    totalShards: process_args && parseInt(process_args[1]) && process_args[0] === "--shard" ? parseInt(process_args[1]) : "auto",
     token: bot.token
-}).on("shardCreate", (shard) => {
-    shardManagerLogger.info(`Creating Shard #${shard.id}`);
 }).spawn().then(() => {
-    shardManagerLogger.sucess("All shards are launched !");
+    console.sucess("All shards are launched !");
 }).catch(err => {
-    return shardManagerLogger.error("An error has occurred ! " + err);
+    return console.error("An error has occurred ! " + err);
 });
 process.on('unhandledRejection', (err) => {
     console.log(err)
